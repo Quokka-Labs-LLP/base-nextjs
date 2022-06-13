4b43f501075f7c7a98ce903160591cc7e8a12134
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-13 19:24:51 +0530

    Implement re-authentication with rtk-query.

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,8 @@
-import { fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
-import { RootState } from './redux/store'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/fetchBaseQuery'
+import { fetchBaseQuery, FetchArgs, retry, BaseQueryFn } from '@reduxjs/toolkit/query/react'
+
+import { RootState } from './redux'
+import { logout, renewToken } from './redux/auth'
 
 export const apiRoot = process.env.REACT_APP_API_ROOT
 
@@ -12,8 +15,27 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set('Authorization', `Bearer ${token}`)
     }
+    headers.set('Accept', '*/*')
+    headers.set('Content-Type', 'application/json')
     return headers
   },
 })
 
-export const baseQueryWithRetry = retry(baseQuery, { maxRetries: 2 })
+// Create our baseQuery instance with Re-Authentication.
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions,
+) => {
+  const result = await baseQuery(args, api, extraOptions)
+
+  if (result.error && result.error.status === 401) {
+    const refreshResult = await baseQuery(`${apiRoot}/refreshToken`, api, extraOptions)
+    const { auth } = api.getState() as RootState
+    refreshResult?.data ? api.dispatch(renewToken({ ...auth, token: 'RENEW_TOKEN' })) : api.dispatch(logout(auth))
+  }
+
+  return result
+}
+
+export const baseQueryWithRetryAndReAuth = retry(baseQueryWithReauth, { maxRetries: 3 })
