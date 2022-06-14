93f896b445c98a9eaba034caf683ed25741a29bc
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-14 15:37:02 +0530

    Reconfigure Prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <>
-      <h1 style={{textAlign: 'center'}}>Welcome to React v18 with Typescript.</h1>
-      <p style={{textAlign: 'center', margin: 0}}>React: v18.1.0</p>
-      <p style={{textAlign: 'center', margin: 0}}>Typescript: v4.7.3</p>
+      <h1 style={{ textAlign: 'center' }}>Welcome to React v18 with Typescript.</h1>
+      <p style={{ textAlign: 'center', margin: 0 }}>React: v18.1.0</p>
+      <p style={{ textAlign: 'center', margin: 0 }}>Typescript: v4.7.3</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
