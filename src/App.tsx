8a69e7b9522253e8032564d1b294476db93bdb42
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-14 15:15:00 +0530

    Upgrade to React V18.1 and Typscript V4.7

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div>
-      React
-    </div>
+    <>
+      <h1 style={{textAlign: 'center'}}>Welcome to React v18 with Typescript.</h1>
+      <p style={{textAlign: 'center', margin: 0}}>React: v18.1.0</p>
+      <p style={{textAlign: 'center', margin: 0}}>Typescript: v4.7.3</p>
+    </>
   );
 }
 
