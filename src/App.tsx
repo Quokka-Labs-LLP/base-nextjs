425c52467ee30188d08f6d102548e8fe0fc88d65
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-15 15:45:05 +0530

    Change default template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import './App.css'
 
-const App: React.FC = () => {
+export default function App(): JSX.Element {
   return (
-    <>
-      <h1 style={{ textAlign: 'center' }}>Welcome to React v18 with Typescript.</h1>
-      <p style={{ textAlign: 'center', margin: 0 }}>React: v18.1.0</p>
-      <p style={{ textAlign: 'center', margin: 0 }}>Typescript: v4.7.3</p>
-    </>
+    <div className='container'>
+      <div className='box'>
+        <h1 className='heading'>Create React App example with Typescript</h1>
+        <p className='paragraph'>
+          <b>Featues: </b>
+        </p>
+        <p className='features'>
+          <p>
+            <b>React:</b> v18.1.0
+          </p>
+          <p>
+            <b>Typescript:</b> v4.7.3
+          </p>
+          <p>
+            <b>Prettier:</b> v2.7.0
+          </p>
+        </p>
+      </div>
+    </div>
   )
 }
-
-export default App
