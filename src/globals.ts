de54ebfd1922b8940c832c9f57cea45ad45b1622
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-15 10:00:13 +0530

    Add bearer token into baseQuery.

--- src/globals.ts
+++ src/globals.ts
@@ -2,13 +2,18 @@ import { FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/fetc
 import { MaybePromise } from '@reduxjs/toolkit/dist/query/tsHelpers'
 import { BaseQueryFn, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
+import { RootState } from './redux'
+
 export const apiRoot = process.env.REACT_APP_API_ROOT
 
 export type BaseQuery = BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError>
 
 export const baseQuery: BaseQuery = fetchBaseQuery({
   baseUrl: apiRoot,
-  prepareHeaders: (headers: Headers): MaybePromise<Headers> => {
+  prepareHeaders: (headers: Headers, { getState }): MaybePromise<Headers> => {
+    const token = (getState() as RootState).auth.token
+
+    token && headers.set('Authorization', `Bearer ${token}`)
     headers.set('Accept', '*/*')
     headers.set('Content-Type', 'application/json')
 
