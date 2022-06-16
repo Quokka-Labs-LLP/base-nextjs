c9cef44e240111006b59ea31b43caa7873df38ab
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-16 16:11:12 +0530

    Reconfigure Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      React
-    </div>
-  );
+  return <div>React</div>
 }
 
-export default App;
+export default App
