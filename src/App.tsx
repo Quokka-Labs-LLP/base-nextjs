8e4a95e0cab781f74a8c1ac67619824c90afd61d
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-06-30 11:10:38 +0530

    Apply the components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-const App: React.FC = () => {
+import './App.css'
+import { Button, Comp, FadeInButton, GlobalStyle, Input, Input1 } from './components'
+
+// Input's attrs will be applied first, and then this attrs obj
+const PasswordInput = styled(Input1).attrs({
+  type: 'password',
+})`
+  // similarly, border will override Input's border
+  border: 2px solid aqua;
+`
+
+export default function App(): JSX.Element {
   return (
-    <div>
-      React
-    </div>
-  );
-}
+    <>
+      <GlobalStyle />
+      <FadeInButton>Fade in Button</FadeInButton>
+      <Comp />
+      <Button>Normal</Button>
+      {/* eslint-disable-next-line */}
+      {/* @ts-ignore */}
+      <Button primary>Primary</Button>
 
-export default App;
+      <Input defaultValue='@probablyup' type='text' />
+      {/* eslint-disable-next-line */}
+      {/* @ts-ignore */}
+      <Input defaultValue='@geelen' type='text' inputColor='rebeccapurple' />
+
+      <Input1 placeholder='A bigger text input' size='2em' />
+      <br />
+      {/* Notice we can still use the size attr from Input */}
+      <PasswordInput placeholder='A bigger password input' size='2em' />
+    </>
+  )
+}
