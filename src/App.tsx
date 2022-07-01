6d1fa328356b96a77bf029959a7d134b9864e7f2
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2022-07-01 15:50:47 +0530

    WEBE-29 #Comment Implement Apollo GraphQL.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,83 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import { gql, useMutation, useSubscription } from '@apollo/client'
 
-const App: React.FC = () => {
-  return (
-    <div>
-      React
-    </div>
-  );
+import './App.css'
+
+const ADD_MESSAGE = gql`
+  mutation AddMessage($text: String!) {
+    createMessage(text: $text) {
+      text
+    }
+  }
+`
+
+const SUB_MESSAGE_ADDED = gql`
+  subscription messageAdded {
+    messageAdded {
+      text
+    }
+  }
+`
+
+export interface Book {
+  title: string
+  author: string
 }
 
-export default App;
+export interface Message {
+  text: string
+}
+
+export default function App(): JSX.Element {
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('')
+  const inputRef = useRef()
+  const subscriptionData = useSubscription(SUB_MESSAGE_ADDED)
+  const [mutateFunction] = useMutation(ADD_MESSAGE)
+
+  useEffect(() => {
+    if (subscriptionData?.data?.messageAdded?.text) {
+      setMessages([subscriptionData.data?.messageAdded as never, ...messages])
+    }
+  }, [subscriptionData.data])
+
+  function sendMessage() {
+    // eslint-disable-next-line
+    // @ts-ignore
+    inputRef.current.value = ''
+    mutateFunction({
+      variables: {
+        text: input,
+      },
+    })
+  }
+
+  return (
+    <div>
+      <h1 className='heading'>React Messaging app</h1>
+      <ul id='messages'>
+        {messages.map((message: Message, idx) => (
+          <li key={idx}>{message.text}</li>
+        ))}
+      </ul>
+      <div id='form'>
+        <input
+          id='input'
+          // eslint-disable-next-line
+          // @ts-ignore
+          ref={inputRef}
+          autoComplete='off'
+          onChange={(e) => {
+            setInput(e.target.value)
+          }}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage()
+            }
+          }}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  )
+}
