700e3286e953f081da1cbc37f0b2a5a3b71a9817
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2023-07-18 12:27:22 +0530

    (fix): resolve errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,84 @@
 
 import { Button, Card, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import Image from 'next/image'
+import styles from './page.module.css'
 
-export default function Home(props: any) {
+export default function Home() {
   const theme = useTheme()
 
   return (
-    <Card>
-      <Button color='primary' variant='contained'>
-        test
-      </Button>
-      <Typography color={`${theme.palette.primary.dark}`} variant='h1'>Hello</Typography>
-    </Card>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        Get started by editing &nbsp;
+        <code className={styles.code}>src/app/page.tsx</code>
+      </div>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src='/next.svg'
+          alt='Next.js Logo'
+          width={180}
+          height={37}
+          priority
+        />
+        <Card>
+          <Button color='primary' variant='contained'>
+            test
+          </Button>
+          <Typography color={`${theme.palette.primary.dark}`} variant='h1'>Hello</Typography>
+        </Card>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className={styles.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className={styles.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className={styles.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className={styles.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+        </a>
+      </div>
+    </main>
   )
 }
