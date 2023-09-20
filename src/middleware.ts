42d805512e0f8a5b80158ca0568352d63c7007b7
vinayak-ql <vinayak@quokkalabs.com>
2023-09-20 14:35:59 +0530

    (feat):Added the card component and improve the functionality on the basis of feedback after demo. remove the commented code.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,19 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   let token: string | undefined
-  // if (request.cookies.has('access_token')) {
-  //   token = request.cookies.get('access_token')?.value
-  // }
+  if (request.cookies.has('access_token')) {
+    token = request.cookies.get('access_token')?.value
+  }
 
-  // token = (request.headers.get('Authorization') as string)?.replace(bearerRegex, 'Bearer')
-  // if (request.nextUrl.pathname.startsWith('/login') && !token) return
+  token = (request.headers.get('Authorization') as string)?.replace(bearerRegex, 'Bearer')
+  if (request.nextUrl.pathname.startsWith('/login') && !token) return
   const response = NextResponse.next()
-  // if (!token) {
-  //   return NextResponse.redirect(new URL('/login', request.url))
-  // }
-  // if ((request.url.includes('/login') && token) || (request.nextUrl.pathname === '/' && token)) {
-  //   return NextResponse.redirect(new URL('/admin/', request.url))
-  // }
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  if ((request.url.includes('/login') && token) || (request.nextUrl.pathname === '/' && token)) {
+    return NextResponse.redirect(new URL('/admin/', request.url))
+  }
   return response
 }
 
